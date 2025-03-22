a07cff1abf9c5007befc605028158efb4e095538
Slava <fjwb@bk.ru>
2025-03-23 00:10:53 +0400

    Полный откат к исходному состоянию

--- Module 3/ReactiveFormLab/vue-project/vite.config.js
+++ Module 3/ReactiveFormLab/vue-project/vite.config.js
@@ -5,15 +5,10 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/FT-LAB-JS/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
   },
 })
