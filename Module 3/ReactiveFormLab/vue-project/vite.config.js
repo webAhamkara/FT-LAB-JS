df61fbee8178a57fd532470a6e22582202a238b1
Slava <fjwb@bk.ru>
2025-03-22 23:44:22 +0400

    fix

--- Module 3/ReactiveFormLab/vue-project/vite.config.js
+++ Module 3/ReactiveFormLab/vue-project/vite.config.js
@@ -4,13 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/FT-LAB-JS/Module 3/ReactiveFormLab/vue-project/dist/',
-  plugins: [vue(), vueDevTools()],
+  base: process.env.NODE_ENV === 'production' ? '/FT-LAB-JS/' : '/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
   },
 })
