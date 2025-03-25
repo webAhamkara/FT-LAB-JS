cbe0416a990b98346ada0eea38f743d9033b9848
Slava <fjwb@bk.ru>
2025-03-25 18:25:11 +0400

    fix

--- docs/Module 3/costAccounting/vue-project/vite.config.js
+++ docs/Module 3/costAccounting/vue-project/vite.config.js
@@ -6,13 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/FT-LAB-JS/vue_pr_two/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
