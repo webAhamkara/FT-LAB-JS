fc4ea3d2ed14ea83e9ff92e34efdbcdf9d2e8f2a
Slava <fjwb@bk.ru>
2025-03-25 18:08:26 +0400

    fix

--- docs/Module 3/ReactiveFormLab/vue-project/vite.config.js
+++ docs/Module 3/ReactiveFormLab/vue-project/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/vue-project/',
+  base: '/FT-LAB-JS/vue-project/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
